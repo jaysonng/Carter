1fbf78719287441289a447c36f4699f647b9543a
Jayson Ng <jayson.ng@gmail.com>
2021-11-23 18:30:56 +0800

    Initial Setup for Carter Swift Package

--- Sources/Carter/Carter.swift
+++ Sources/Carter/Carter.swift
@@ -1,9 +1,13 @@
 import Foundation
 
 public struct Carter {
-    public let url: URL
-
+    //public let url: URL
+    
+    public init() {
+        
+    }
+    
     public init(_ url: URL) {
-        self.url = url
+        //self.url = url
     }
 }
--- Tests/CarterTests/CarterTests.swift
+++ Tests/CarterTests/CarterTests.swift
@@ -1,11 +1,7 @@
+import Foundation
 import XCTest
 @testable import Carter
 
 final class CarterTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Carter().text, "Hello, World!")
-    }
+
 }
